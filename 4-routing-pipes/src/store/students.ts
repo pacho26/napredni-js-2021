344d6c3ed54e8d11cdbe78a4890724ca646a1eb1
Patrik Slovic <patrikslovic@gmail.com>
2021-12-07 15:00:51 +0100

    Add files via upload

---
+++ 4-routing-pipes/src/store/students.ts
@@ -0,0 +1,33 @@
+export const students = [
+  {
+    ime: 'Marian',
+    jmbag: '0246045678',
+    ects: 62,
+    prosjek: 4.64,
+  },
+  {
+    ime: 'Tomislav',
+    jmbag: '0246046593',
+    ects: 54,
+    prosjek: 3.8,
+  },
+
+  {
+    ime: 'Matija',
+    jmbag: '0246012345',
+    ects: 61,
+    prosjek: 4.86,
+  },
+  {
+    ime: 'Patrik',
+    jmbag: '0246090045',
+    ects: 24,
+    prosjek: 4.13,
+  },
+  {
+    ime: 'Ognjen',
+    jmbag: '0246657481',
+    ects: 124,
+    prosjek: 5.0,
+  },
+];
